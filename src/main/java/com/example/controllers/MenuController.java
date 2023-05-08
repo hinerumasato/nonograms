d371059321bb109933cc681a204f28d63ec24357
Hoang Hai Van <hoanghaivan456@gmail.com>
2023-05-09 00:32:09 +0700

    Add MenuController, Update Content of MenuView

--- src/main/java/com/example/controllers/MenuController.java
+++ src/main/java/com/example/controllers/MenuController.java
@@ -1,6 +1,16 @@
 package com.example.controllers;
 
+import java.io.IOException;
+
+import com.example.models.FXMLFile;
+import com.example.models.ImgFile;
+import com.example.models.LevelModel;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 public class MenuController {
     private Button easyButton;
@@ -11,6 +21,35 @@ public class MenuController {
         this.easyButton = easyButton;
         this.mediumButton = mediumButton;
         this.hardButton = hardButton;
+    }
+
+    private void loadGame(LevelModel level) {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(new FXMLFile(level.getLevel()).URLLoad());
+            Scene scene = new Scene(loader.load());
+
+            stage.setScene(scene);
+            stage.getIcons().add(new Image(new ImgFile("app-icon").load()));
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initialize() {
+        easyButton.setOnAction(event -> {
+            loadGame(LevelModel.EASY);
+        });
+
+        mediumButton.setOnAction(event -> {
+            loadGame(LevelModel.MEDIUM);
+        });
+
+        hardButton.setOnAction(event -> {
+            loadGame(LevelModel.HARD);
+        });
     }
 
 }
--- src/main/java/com/example/views/MenuView.java
+++ src/main/java/com/example/views/MenuView.java
@@ -1,18 +1,13 @@
 package com.example.views;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.example.models.FXMLFile;
-import com.example.models.LevelModel;
+import com.example.controllers.MenuController;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
 
 public class MenuView implements Initializable {
 
@@ -25,33 +20,12 @@ public class MenuView implements Initializable {
     @FXML
     private Button hardButton;
 
-    private void loadGame(LevelModel level) {
-        try {
-            Stage stage = new Stage();
-            FXMLLoader loader = new FXMLLoader(new FXMLFile(level.getLevel()).URLLoad());
-            Scene scene = new Scene(loader.load());
-
-            stage.setScene(scene);
-            stage.setResizable(false);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    private MenuController menuController;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        easyButton.setOnAction(event -> {
-            loadGame(LevelModel.EASY);
-        });
-
-        mediumButton.setOnAction(event -> {
-            loadGame(LevelModel.MEDIUM);
-        });
-
-        hardButton.setOnAction(event -> {
-            loadGame(LevelModel.HARD);
-        });
+        this.menuController = new MenuController(easyButton, mediumButton, hardButton);
+        menuController.initialize();
     }
 
 }
