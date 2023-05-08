dbc644a1e623b625c8c56020501ccc79fbb8e896
Thang Loi <thangloitran406@gmail.com>
2023-05-09 00:25:46 +0700

    Fix push before

---
+++ src/main/java/com/example/controllers/MenuController.java
@@ -0,0 +1,16 @@
+package com.example.controllers;
+
+import javafx.scene.control.Button;
+
+public class MenuController {
+    private Button easyButton;
+    private Button mediumButton;
+    private Button hardButton;
+
+    public MenuController(Button easyButton, Button mediumButton, Button hardButton) {
+        this.easyButton = easyButton;
+        this.mediumButton = mediumButton;
+        this.hardButton = hardButton;
+    }
+
+}
